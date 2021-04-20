b0871ac133b6b2abff80f02628ea21ba0675f929
Damian Paszkowski <paszko.damian@gmail.com>
2021-04-20 11:01:00 +0200

    fix api view

--- src/borrow/migrations/005-liq2-0.sql
+++ src/borrow/migrations/005-liq2-0.sql
@@ -89,9 +89,8 @@ ALTER TABLE vault.events ADD collateral_taken decimal(78,18) NULL;
 CREATE INDEX event_auction_id ON vault.events(auction_id);
 
 CREATE VIEW api.vault_events AS (
-    SELECT e.*, t.hash, COALESCE(c.cdp_id, null)
+    SELECT e.*, t.hash
     FROM vault.events e 
     JOIN vulcan2x.transaction t ON e.tx_id = t.id
-    JOIN manager.cdp c ON c.urn = e.urn
     ORDER BY timestamp ASC, block_id ASC, log_index ASC
 );
