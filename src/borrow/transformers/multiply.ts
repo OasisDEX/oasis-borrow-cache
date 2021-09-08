d8c53843e235142bdd1a6bcbb1f0d0ddbf496ab8
Damian Paszkowski <paszko.damian@gmail.com>
2021-09-08 14:22:18 +0200

    add liqidation ratio to multiply events

--- src/borrow/transformers/multiply.ts
+++ src/borrow/transformers/multiply.ts
@@ -10,34 +10,77 @@ import { LocalServices } from '@oasisdex/spock-etl/dist/services/types';
 
 import { Dictionary } from 'ts-essentials';
 import { normalizeAddressDefinition } from '../../utils';
+import { getLiquidationRatio } from '../dependencies/getLiquidationRatio';
+import { getIlkForCdp } from '../dependencies/getIlkForCdp';
+import { cleanUpString } from '../../utils/cleanUpString';
 
 const multiplyAbi = require('../../../abis/multiply-proxy-actions.json');
 
-const handleMultiply = async (params: Dictionary<any>, log: PersistedLog, services: LocalServices) => {
+interface Dependencies {
+  getIlkForCdp: typeof getIlkForCdp
+  getLiquidationRatio: typeof getLiquidationRatio,
+  cdpManager: string,
+  vat: string,
+}
+
+const handleMultiply = async (params: Dictionary<any>, log: PersistedLog, services: LocalServices, dependencies: Dependencies) => {
+  const cdpId = params.cdpId.toString();
+  const block = await services.tx.oneOrNone(
+    `
+    select * from vulcan2x.block where id = ${log.block_id}
+    `
+  )
+
+  const ilk = await dependencies.getIlkForCdp(
+    cdpId,
+    services
+  )
+
+  const liquidationRatio = await dependencies.getLiquidationRatio(
+    ilk,
+    block.number,
+    services,
+  )
+  
   const values = {
     method_name: params.methodName,
-    cdp_id: params.cdpId.toString(),
+    cdp_id: cdpId,
     swap_min_amount: params.swapMinAmount.toString(),
     swap_optimist_amount: params.swapOptimistAmount.toString(),
     collateral_left: params.collateralLeft.toString(),
     dai_left: params.daiLeft.toString(),
-    urn: '',
+    ilk: cleanUpString(ilk),
+    liquidation_ratio: liquidationRatio.toNumber(),
 
     log_index: log.log_index,
     tx_id: log.tx_id,
     block_id: log.block_id,
   };
 
-  await services.tx.none(
-    `INSERT INTO multiply.method_called(
-          method_name, cdp_id, swap_min_amount, swap_optimist_amount, collateral_left, dai_left, urn,
-          log_index, tx_id, block_id
-        ) VALUES (
-          \${method_name}, \${cdp_id}, \${swap_min_amount}, \${swap_optimist_amount}, \${collateral_left}, \${dai_left}, \${urn},
-          \${log_index}, \${tx_id}, \${block_id}
-        );`,
-    values,
+  const cs = new services.pg.helpers.ColumnSet(
+    [
+      'method_name',
+      'cdp_id',
+      'swap_min_amount',
+      'swap_optimist_amount',
+      'collateral_left',
+      'dai_left',
+      'liquidation_ratio',
+      'ilk',
+      'tx_id',
+      'block_id',
+      'log_index',
+    ],
+    {
+      table: {
+          schema: 'multiply',
+          table: 'method_called',
+      },
+    },
   );
+
+  const query = services.pg.helpers.insert([values], cs);
+  await services.tx.none(query);
 };
 
 const handleFL = async (params: Dictionary<any>, log: PersistedLog, services: LocalServices) => {
@@ -62,27 +105,29 @@ const handleFL = async (params: Dictionary<any>, log: PersistedLog, services: Lo
       );
     };
 
-const handlers = {
+const handlers = (dependencies: Dependencies) => ({
   async MultipleActionCalled(services: LocalServices, { event, log }: FullEventInfo): Promise<void> {
-    await handleMultiply(event.params, log, services);
+    await handleMultiply(event.params, log, services,dependencies);
   },
   async FLData(services: LocalServices, { event, log }: FullEventInfo): Promise<void> {
     await handleFL(event.params, log, services);
   },
-};
+});
 
 export const multiplyTransformer: (
     addresses: (string | SimpleProcessorDefinition)[],
-  ) => BlockTransformer[] = (addresses) => {
+    dependencies: Dependencies
+  ) => BlockTransformer[] = (addresses, dependencies) => {
     return addresses.map(_deps => {
       const deps = normalizeAddressDefinition(_deps);
   
       return {
         name: `multiplyActions-${deps.address}`,
         dependencies: [getExtractorName(deps.address)],
+        transformerDependencies: [`openCdpTransformer-${dependencies.cdpManager}`, `vatTransformer-${dependencies.vat}`],
         startingBlock: deps.startingBlock,
         transform: async (services, logs) => {
-          await handleEvents(services, multiplyAbi, flatten(logs), handlers);
+          await handleEvents(services, multiplyAbi, flatten(logs), handlers(dependencies));
         },
       };
     });
