6b42acd36fac6271ebe2fcbc32c6fa3d50550d38
johnnieskywalker <lwolski0@gmail.com>
2021-09-23 12:04:04 +0200

    added configs for goerli

---
+++ src/config.goerli.ts
@@ -0,0 +1,170 @@
+import { makeRawLogExtractors } from '@oasisdex/spock-utils/dist/extractors/rawEventDataExtractor';
+import { makeRawEventBasedOnTopicExtractor } from '@oasisdex/spock-utils/dist/extractors/rawEventBasedOnTopicExtractor';
+import { join } from 'path';
+
+import { UserProvidedSpockConfig } from '@oasisdex/spock-etl/dist/services/config';
+import {
+  managerGiveTransformer,
+  openCdpTransformer,
+} from './borrow/transformers/cdpManagerTransformer';
+
+import {
+  vatCombineTransformer,
+  vatMoveEventsTransformer,
+  vatRawMoveTransformer,
+  vatTransformer,
+} from './borrow/transformers/vatTransformer';
+import { auctionTransformer, catTransformer } from './borrow/transformers/catTransformer';
+import {
+  AbiInfo,
+  makeRawEventExtractorBasedOnTopicIgnoreConflicts,
+  makeRowEventBasedOnDSNoteTopic,
+} from './borrow/customExtractors';
+import { flipNoteTransformer, flipTransformer } from './borrow/transformers/flipperTransformer';
+import { getIlkInfo } from './borrow/dependencies/getIlkInfo';
+import { getUrnForCdp } from './borrow/dependencies/getUrnForCdp';
+import {
+  auctionLiq2Transformer,
+  dogTransformer,
+  getDogTransformerName,
+} from './borrow/transformers/dogTransformer';
+import { clipperTransformer } from './borrow/transformers/clipperTransformer';
+
+import { getOraclesAddresses } from './utils/addresses';
+import {
+  getOracleTransformerName,
+  oraclesTransformer,
+} from './borrow/transformers/oraclesTransformer';
+import { eventEnhancerTransformer } from './borrow/transformers/eventEnhancer';
+
+const goerliAddresses = require('./addresses/goerli.json');
+
+const GENESIS = 5273074;
+
+const vat = {
+  address: goerliAddresses.MCD_VAT,
+  startingBlock: GENESIS,
+};
+
+const cdpManagers = [
+  {
+    address: goerliAddresses.CDP_MANAGER,
+    startingBlock: 5273301,
+  },
+];
+//
+const cats = [
+  {
+    address: goerliAddresses.MCD_CAT,
+    startingBlock: 5273080,
+  },
+];
+
+const dogs = [
+  {
+    address: goerliAddresses.MCD_DOG,
+    startingBlock: 5273080,
+  },
+];
+
+const clippers = [
+  {
+    name: 'clipper',
+    abi: require('../abis/clipper.json'),
+    startingBlock: GENESIS,
+  },
+];
+
+const flipper = [
+  {
+    name: 'flipper',
+    abi: require('../abis/flipper.json'),
+    startingBlock: GENESIS,
+  },
+];
+
+const oracle = [
+  {
+    name: 'oracle',
+    abi: require('../abis/oracle.json'),
+    startingBlock: GENESIS,
+  },
+  {
+    name: 'lp-oracle',
+    abi: require('../abis/lp-oracle.json'),
+    startingBlock: GENESIS,
+  },
+];
+
+const flipperNotes: AbiInfo[] = [
+  {
+    name: 'flipper',
+    functionNames: [
+      'tend(uint256,uint256,uint256)',
+      'dent(uint256,uint256,uint256)',
+      'deal(uint256)',
+    ],
+    abi: require('../abis/flipper.json'),
+    startingBlock: GENESIS,
+  },
+];
+
+const addresses = {
+  ...goerliAddresses,
+  //   LW - to investigate don't know migration address for goerli
+  MIGRATION: '',
+  //   TODO - LW going to check if this is required for Goerli
+  //   ILK_REGISTRY: '0x5a464C28D19848f44199D003BeF5ecc87d090F87',
+};
+
+const oracles = getOraclesAddresses(goerliAddresses).map(description => ({
+  ...description,
+  startingBlock: GENESIS,
+}));
+
+const oraclesTransformers = oracles.map(getOracleTransformerName);
+
+export const config: UserProvidedSpockConfig = {
+  startingBlock: GENESIS,
+  extractors: [
+    ...makeRawLogExtractors(cdpManagers),
+    ...makeRawLogExtractors(cats),
+    ...makeRawLogExtractors(dogs),
+    ...makeRawLogExtractors([vat]),
+    ...makeRawEventBasedOnTopicExtractor(flipper),
+    ...makeRowEventBasedOnDSNoteTopic(flipperNotes),
+    ...makeRawEventExtractorBasedOnTopicIgnoreConflicts(
+      clippers,
+      dogs.map(dog => dog.address.toLowerCase()),
+    ), // ignore dogs addresses because event name conflict
+    ...makeRawEventExtractorBasedOnTopicIgnoreConflicts(oracle),
+  ],
+  transformers: [
+    ...openCdpTransformer(cdpManagers, { getUrnForCdp }),
+    ...managerGiveTransformer(cdpManagers),
+    ...catTransformer(cats),
+    ...auctionTransformer(cats, { getIlkInfo }),
+    ...dogTransformer(dogs),
+    ...auctionLiq2Transformer(dogs, { getIlkInfo }),
+    vatTransformer(vat),
+    vatCombineTransformer(vat),
+    vatMoveEventsTransformer(vat),
+    vatRawMoveTransformer(vat),
+    flipTransformer(),
+    flipNoteTransformer(),
+    clipperTransformer(dogs.map(dep => getDogTransformerName(dep.address))),
+    ...oraclesTransformer(oracles),
+    eventEnhancerTransformer(vat.address, GENESIS, oraclesTransformers),
+  ],
+  migrations: {
+    borrow: join(__dirname, './borrow/migrations'),
+  },
+  api: {
+    whitelisting: {
+      enabled: false,
+      whitelistedQueriesDir: './queries',
+    },
+  },
+  addresses,
+  onStart: () => {},
+};
