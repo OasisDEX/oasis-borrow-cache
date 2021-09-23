6b068eef8779a428b4fd13d57882a79553aabfcc
johnnieskywalker <lwolski0@gmail.com>
2021-09-23 12:38:33 +0200

    Revert "ran linter"
    This reverts commit 560e76529e68529aecec44fbc9b6f19089eb1434.

--- src/borrow/transformers/oraclesTransformer.ts
+++ src/borrow/transformers/oraclesTransformer.ts
@@ -1,10 +1,10 @@
 import { flatten } from 'lodash';
 
-import { handleEvents, FullEventInfo } from '@oasisdex/spock-utils/dist/transformers/common';
 import {
-  PersistedLog,
-  SimpleProcessorDefinition,
-} from '@oasisdex/spock-utils/dist/extractors/rawEventDataExtractor';
+  handleEvents,
+  FullEventInfo,
+} from '@oasisdex/spock-utils/dist/transformers/common';
+import { PersistedLog, SimpleProcessorDefinition } from '@oasisdex/spock-utils/dist/extractors/rawEventDataExtractor';
 
 import { BlockTransformer } from '@oasisdex/spock-etl/dist/processors/types';
 import { LocalServices } from '@oasisdex/spock-etl/dist/services/types';
@@ -18,37 +18,45 @@ const oracleAbi = require('../../../abis/oracle.json');
 const lpOracleAbi = require('../../../abis/lp-oracle.json');
 
 interface Price {
-  price: string;
-  token: string;
-  timestamp: string;
-  osm_address: string;
-  log_index: number;
-  tx_id: number;
-  block_id: number;
+  price: string
+  token: string
+  timestamp: string,
+  osm_address: string,
+  log_index: number,
+  tx_id: number,
+  block_id: number,
 }
 
 function isLPToken(token: string) {
-  return token.startsWith('UNIV2');
+  return token.startsWith('UNIV2')
 }
 
 async function getTimeStamp(services: LocalServices, block_id: number): Promise<string> {
   const value = await services.tx.oneOrNone(
     `SELECT timestamp FROM vulcan2x.block WHERE id = \${block_id}`,
     {
-      block_id: block_id,
+        block_id: block_id,
     },
-  );
+  )
 
-  return value.timestamp;
+  return value.timestamp
 }
 
 async function savePriceToDb(services: LocalServices, row: Price) {
   const cs = new services.pg.helpers.ColumnSet(
-    ['price', 'token', 'timestamp', 'osm_address', 'tx_id', 'block_id', 'log_index'],
+    [
+      'price',
+      'token',
+      'timestamp',
+      'osm_address',
+      'tx_id',
+      'block_id',
+      'log_index',
+    ],
     {
       table: {
-        schema: 'oracles',
-        table: 'prices',
+          schema: 'oracles',
+          table: 'prices',
       },
     },
   );
@@ -57,13 +65,8 @@ async function savePriceToDb(services: LocalServices, row: Price) {
   await services.tx.none(query);
 }
 
-const savePrice = async (
-  services: LocalServices,
-  log: PersistedLog,
-  price: BigNumber,
-  token: string,
-) => {
-  const timestamp = await getTimeStamp(services, log.block_id);
+const savePrice = async (services: LocalServices, log: PersistedLog, price: BigNumber, token: string) => {
+  const timestamp = await getTimeStamp(services, log.block_id)
   const row = {
     price: price.toString(),
     token,
@@ -72,27 +75,27 @@ const savePrice = async (
     log_index: log.log_index,
     tx_id: log.tx_id,
     block_id: log.block_id,
-  };
-  await savePriceToDb(services, row);
-};
+  }
+  await savePriceToDb(services, row)
+}
 
 const handlers = (token: string) => ({
   async LogValue(services: LocalServices, { event, log }: FullEventInfo): Promise<void> {
-    const price = new BigNumber(event.params.val).div(wad);
-    await savePrice(services, log, price, token);
+    const price = new BigNumber(event.params.val).div(wad)
+    await savePrice(services, log, price, token)
   },
   async Value(services: LocalServices, { event, log }: FullEventInfo): Promise<void> {
-    const price = new BigNumber(event.params.curVal).div(wad);
-    await savePrice(services, log, price, token);
+    const price = new BigNumber(event.params.curVal).div(wad)
+    await savePrice(services, log, price, token)
   },
-});
+})
 
-export function getOracleTransformerName(oracle: SimpleProcessorDefinition & { token: string }) {
-  return `oracles_transformer_${oracle.address}_${oracle.token}`;
+export function getOracleTransformerName(oracle: SimpleProcessorDefinition & {token: string}) {
+  return `oracles_transformer_${oracle.address}_${oracle.token}`
 }
 
 export const oraclesTransformer: (
-  addresses: (SimpleProcessorDefinition & { token: string })[],
+  addresses: (SimpleProcessorDefinition & {token: string})[],
 ) => BlockTransformer[] = addresses => {
   return addresses.map(_deps => {
     const deps = normalizeAddressDefinition(_deps);
@@ -100,14 +103,12 @@ export const oraclesTransformer: (
     return {
       name: getOracleTransformerName(_deps),
       dependencies: [
-        getCustomExtractorNameBasedOnDSNoteTopicIgnoreConflicts('oracle'),
-        getCustomExtractorNameBasedOnDSNoteTopicIgnoreConflicts('lp-oracle'),
+        getCustomExtractorNameBasedOnDSNoteTopicIgnoreConflicts('oracle'), 
+        getCustomExtractorNameBasedOnDSNoteTopicIgnoreConflicts('lp-oracle')
       ],
       startingBlock: deps.startingBlock,
       transform: async (services, logs) => {
-        const logsFromOSM = flatten(logs).filter(
-          log => log.address.toLowerCase() === _deps.address.toLowerCase(),
-        );
+        const logsFromOSM = flatten(logs).filter(log => log.address.toLowerCase() === _deps.address.toLowerCase())
         const abi = isLPToken(_deps.token) ? lpOracleAbi : oracleAbi;
         await handleEvents(services, abi, logsFromOSM, handlers(_deps.token));
       },
--- src/utils/cleanUpString.ts
+++ src/utils/cleanUpString.ts
@@ -1,4 +1,4 @@
-// when the EVM converts bytes to string we can end up with invisible null characters that prevent us from saving it to db
+// when the EVM converts bytes to string we can end up with invisible null characters that prevent us from saving it to db 
 // e.g. 'ETH-A%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00%00'
 
 export function cleanUpString(str: string): string {
