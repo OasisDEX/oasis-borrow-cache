210d3ce22e96cdd4bb9c8f0ea576fd12fbfc2561
Damian Paszkowski <paszko.damian@gmail.com>
2021-10-14 08:46:12 +0100

    add missing fields to view

--- src/borrow/migrations/010-multiplyEvent.sql
+++ src/borrow/migrations/010-multiplyEvent.sql
@@ -54,9 +54,17 @@ CREATE VIEW api.vault_events AS (
 
 CREATE VIEW api.vault_multiply_history AS (
     SELECT 
-    t.hash, b.timestamp,
-    me.urn, me.kind, me.market_price, me.oracle_price, me.before_collateral as before_locked_collateral, me.collateral as locked_collateral, me.before_collateralization_ratio, me.collateralization_ratio, me.before_debt, me.debt, me.before_multiple, me.multiple, me.before_liquidation_price, me.liquidation_price, me.net_value, me.oazo_fee, me.loan_fee, me.gas_fee, me.total_fee, me.bought, me.deposit_collateral, me.deposit_dai, me.sold, me.withdrawn_collateral, me.withdrawn_dai, me.exit_collateral, me.exit_dai,
-    e.collateral_amount, e.dai_amount, e.rate, e.vault_creator, e.depositor, e.cdp_id, e.transfer_from, e.transfer_to, e.collateral, e.auction_id, e.liq_penalty, e.collateral_price, e.covered_debt, e.remaining_debt, e.remaining_collateral, e.collateral_taken, e.ilk
+    t.hash as tx_hash, b.timestamp,
+    me.urn, me.kind, me.market_price, me.oracle_price, me.before_collateral as before_locked_collateral,
+    me.collateral as locked_collateral, me.before_collateralization_ratio, me.collateralization_ratio, 
+    me.before_debt, me.debt, me.before_multiple, me.multiple, me.before_liquidation_price, me.liquidation_price, 
+    me.net_value, me.oazo_fee, me.loan_fee, me.gas_fee, me.total_fee, me.bought, me.deposit_collateral, 
+    me.deposit_dai, me.sold, me.withdrawn_collateral, me.withdrawn_dai, me.exit_collateral, me.exit_dai,
+    me.tx_id, me.log_index, me.block_id,
+    b.number as block_number, b.hash as block_hash,
+    e.collateral_amount, e.dai_amount, e.rate, e.vault_creator, e.depositor, e.cdp_id, e.transfer_from,
+    e.transfer_to, e.collateral, e.auction_id, e.liq_penalty, e.collateral_price, e.covered_debt, 
+    e.remaining_debt, e.remaining_collateral, e.collateral_taken, e.ilk
     FROM vault.multiply_events me
     JOIN vulcan2x.transaction t ON me.tx_id = t.id
     JOIN vulcan2x.block b ON me.block_id = b.id
