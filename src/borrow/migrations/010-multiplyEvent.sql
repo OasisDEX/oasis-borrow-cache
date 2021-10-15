2c241ad0af92165115adfe25dba2b9cf5acd622e
Damian Paszkowski <paszko.damian@gmail.com>
2021-10-15 08:18:03 +0100

    fix issue with unique constrain, two Move events

--- src/borrow/migrations/010-multiplyEvent.sql
+++ src/borrow/migrations/010-multiplyEvent.sql
@@ -37,7 +37,7 @@ CREATE TABLE vault.multiply_events (
     log_index                       integer not null,
     tx_id                           integer not null REFERENCES vulcan2x.transaction(id) ON DELETE CASCADE,
     block_id                        integer not null REFERENCES vulcan2x.block(id) ON DELETE CASCADE,
-    unique (tx_id, log_index)
+    unique (tx_id, log_index, kind)
 );
 
 DROP VIEW api.vault_events;
