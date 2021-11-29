77f9befdcb0d3b51b3246599b0f3f39e655819be
Damian Paszkowski <paszko.damian@gmail.com>
2021-11-29 23:24:45 +0000

    add migration

--- src/borrow/migrations/012-fix-dai-left.sql
+++ src/borrow/migrations/012-fix-dai-left.sql
@@ -2,3 +2,11 @@ UPDATE vault.multiply_events
     SET exit_dai = (SELECT dai_left FROM multiply.method_called WHERE vault.multiply_events.tx_id = tx_id) * 10^(-18) 
     WHERE kind = 'CLOSE_VAULT_TO_COLLATERAL';
 
+UPDATE vault.events SET 
+	oracle_price = (
+		select price from oracles.prices 
+		where oracles.prices.block_id <= vault.events.block_id
+		and oracles.prices.token = vault.events.collateral
+		order by block_id DESC
+		limit 1
+	) where kind = 'AUCTION_STARTED_V2' OR kind = 'AUCTION_STATED';
