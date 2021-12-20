fef0879eff3c86cf8e92e2a6e4b5320f2cb98a95
sebastianPiekarczyk <sebastian@oazoapps.com>
2021-12-20 11:53:46 +0100

    added semicolons

--- src/borrow/migrations/014-guni-update.sql
+++ src/borrow/migrations/014-guni-update.sql
@@ -1,3 +1,3 @@
-DELETE FROM vault.multiply_events WHERE block_id in (SELECT block_id from vault.multiply_events vme JOIN vulcan2x.block vb ON vb.id = vme.block_id WHERE vb.number > 13733654)
+DELETE FROM vault.multiply_events WHERE block_id in (SELECT block_id from vault.multiply_events vme JOIN vulcan2x.block vb ON vb.id = vme.block_id WHERE vb.number > 13733654);
 
-UPDATE vulcan2x.job vj SET last_block_id = 13733654 WHERE vj.name = 'multiply-history'
+UPDATE vulcan2x.job vj SET last_block_id = 13733654 WHERE vj.name = 'multiply-history';
