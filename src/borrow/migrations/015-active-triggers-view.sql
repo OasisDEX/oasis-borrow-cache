eae98403768fe212c238e623a32997e17cc25276
Roman Krasiuk <rokrassyuk@gmail.com>
2022-01-18 13:09:46 -0800

    revert change to 015 migration

--- src/borrow/migrations/015-active-triggers-view.sql
+++ src/borrow/migrations/015-active-triggers-view.sql
@@ -3,3 +3,4 @@ CREATE VIEW api.active_triggers as
      FROM automation_bot.trigger_added_events added
      LEFT JOIN automation_bot.trigger_removed_events removed ON added.trigger_id = removed.trigger_id
      WHERE removed.trigger_id is null;
+     
