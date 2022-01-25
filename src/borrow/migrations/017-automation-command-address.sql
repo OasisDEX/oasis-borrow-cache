588af0c178daf121227c6bf85bfa5a094a5b99f8
Roman Krasiuk <rokrassyuk@gmail.com>
2022-01-25 17:03:17 +0100

    migration

--- src/borrow/migrations/017-automation-command-address.sql
+++ src/borrow/migrations/017-automation-command-address.sql
@@ -1,5 +1,13 @@
+DROP VIEW api.active_triggers;
+
 ALTER TABLE automation_bot.trigger_added_events
     DROP COLUMN trigger_type;
 
 ALTER TABLE automation_bot.trigger_added_events
     ADD COLUMN command_address character varying(66) not null;
+
+CREATE VIEW api.active_triggers as 
+    SELECT added.*
+    FROM automation_bot.trigger_added_events added
+    LEFT JOIN automation_bot.trigger_removed_events removed ON added.trigger_id = removed.trigger_id
+    WHERE removed.trigger_id is null;
