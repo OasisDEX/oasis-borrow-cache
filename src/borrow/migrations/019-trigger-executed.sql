0f800b4a9f6d84e42cc966b62ba39614387879ac
johnnieskywalker <lwolski0@gmail.com>
2022-02-08 17:08:18 +0100

    formatted sql file

--- src/borrow/migrations/019-trigger-executed.sql
+++ src/borrow/migrations/019-trigger-executed.sql
@@ -1,11 +1,12 @@
-CREATE TABLE automation_bot.trigger_executed_events (
-    id  serial primary key,
-    cdp_id                  decimal(78,0) not null,
-    trigger_id decimal(78,0) not null,
-
-    log_index               integer not null,
-    tx_id                   integer not null REFERENCES vulcan2x.transaction(id) ON DELETE CASCADE,
-    block_id                integer not null REFERENCES vulcan2x.block(id) ON DELETE CASCADE,
-
-    unique (tx_id, log_index)
-)
+CREATE TABLE
+    automation_bot.trigger_executed_events (
+        id serial PRIMARY key,
+        cdp_id DECIMAL(78, 0) NOT NULL,
+        trigger_id DECIMAL(78, 0) NOT NULL,
+        log_index INTEGER NOT NULL,
+        tx_id INTEGER NOT NULL REFERENCES vulcan2x.transaction(id)
+        ON DELETE CASCADE,
+        block_id INTEGER NOT NULL REFERENCES vulcan2x.block(id)
+        ON DELETE CASCADE,
+        UNIQUE (tx_id, log_index)
+    )
