94f8b9ed0dbb958903f6832ea0009090dc669da7
johnnieskywalker <lwolski0@gmail.com>
2022-03-15 18:54:17 +0100

    added columns kind and event_type in all trigger event tables and created a view unioning them all

---
+++ src/borrow/migrations/022-automation-events-ddl-dml-kind-type.sql
@@ -0,0 +1,50 @@
+ALTER TABLE automation_bot.trigger_added_events
+			ADD column kind character varying (66);
+		
+UPDATE automation_bot.trigger_added_events 
+		SET kind = 'stop-loss'
+		WHERE kind = null ;
+	
+ALTER TABLE automation_bot.trigger_removed_events 
+			ADD column kind character varying (66);
+		
+UPDATE automation_bot.trigger_removed_events 
+		SET kind = 'stop-loss'
+		WHERE kind = null ;
+	
+ALTER TABLE automation_bot.trigger_executed_events  
+			ADD column kind character varying (66);
+		
+UPDATE automation_bot.trigger_executed_events  
+		SET kind = 'stop-loss'
+		WHERE kind = null ;
+
+ALTER TABLE automation_bot.trigger_added_events 
+	ADD column event_type character varying (66);
+
+UPDATE automation_bot.trigger_added_events 
+		SET event_type = 'added'
+		WHERE kind = null ;
+	
+ALTER TABLE automation_bot.trigger_removed_events  
+	ADD column event_type character varying (66);
+
+UPDATE automation_bot.trigger_removed_events 
+		SET event_type = 'removed'
+		WHERE kind = null ;	
+	
+ALTER TABLE automation_bot.trigger_executed_events 
+	ADD column event_type character varying (66);
+
+UPDATE automation_bot.trigger_executed_events
+		SET event_type = 'executed'
+		WHERE kind = null ;	
+
+CREATE view api.trigger_events as (
+	SELECT id , trigger_id , cdp_id, tx_id , block_id, kind , event_type  from automation_bot.trigger_added_events tae
+	UNION
+	SELECT  id , trigger_id , cdp_id, tx_id , block_id, kind , event_type  from automation_bot.trigger_removed_events tre 
+	UNION 
+	SELECT  id , trigger_id , cdp_id, tx_id , block_id, kind , event_type  from automation_bot.trigger_executed_events tee 
+)
+	
