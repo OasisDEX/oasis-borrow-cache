222c54873dcf338057ace77b9fac95cfa494fd78
johnnieskywalker <lwolski0@gmail.com>
2022-03-17 15:12:04 +0100

    fixed ON typo

--- src/borrow/migrations/022-automation-events-ddl-kind-type.sql
+++ src/borrow/migrations/022-automation-events-ddl-kind-type.sql
@@ -6,7 +6,7 @@
  	kind varchar(66) NULL
  );
 
-CREATE VIEW api.trigger_events AS 
+CREATE VIEW api.trigger_events AS (
 	SELECT id , trigger_id , cdp_id, tx_id , block_id , 'added' AS event_type, tae.command_address,  alias.kind  from automation_bot.trigger_added_events tae
 	LEFT JOIN automation_bot.command_alias alias
 	ON tae.command_address = alias.command_address
@@ -14,11 +14,12 @@ CREATE VIEW api.trigger_events AS
 	SELECT  tre.id , tre.trigger_id , tre.cdp_id, tre.tx_id , tre.block_id , 'removes' AS event_type, tae.command_address, alias.kind  from automation_bot.trigger_removed_events tre 
 	LEFT JOIN automation_bot.trigger_added_events  tae
 	ON tre.id = tae.id
-	LEFT JOIN automatiON_bot.command_alias alias
+	LEFT JOIN automation_bot.command_alias alias
 	ON tae.command_address = alias.command_address
 	UNION
 	SELECT  tee.id , tee.trigger_id , tee.cdp_id, tee.tx_id , tee.block_id , 'executed' AS event_type, tae.command_address,  alias.kind  from automation_bot.trigger_executed_events tee
 	LEFT JOIN automation_bot.trigger_added_events  tae
 	ON tee.id = tae.id
 	LEFT JOIN automation_bot.command_alias alias
-	ON tae.command_address = alias.command_address;
+	ON tae.command_address = alias.command_address
+);
