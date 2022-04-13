ea53f1592eb03511a70b5079c8b5fee98c74ce90
Adam Skrodzki <adam@oazoapps.com>
2022-04-13 13:06:51 +0200

    automation update

---
+++ src/borrow/migrations/024-clear-automation-bot.sql
@@ -0,0 +1,5 @@
+DELETE FROM automation_bot.trigger_added_events;
+DELETE FROM automation_bot.trigger_removed_events;
+DELETE FROM automation_bot.approval_granted_events;
+DELETE FROM automation_bot.approval_removed_events;
+DELETE FROM automation_bot.trigger_executed_events;
