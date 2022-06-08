3c473c82d4ced9bd27f1c29de49b67d7cbcf41c1
sennett <anthony@oazoapps.com>
2022-06-08 10:44:10 +0100

    performant historic price view

--- src/borrow/migrations/026-create-historical-prices.sql
+++ src/borrow/migrations/026-create-historical-prices.sql
@@ -1,59 +1,51 @@
-CREATE VIEW api.historic_token_prices as (
-    with token as (
-        select distinct token
-        from oracles.prices
-    ),
-    price as (
-        select distinct on(token) token,
-            price,
-            timestamp as timestamp
-        from oracles.prices
-        where timestamp > current_date - interval '2 days'
-        order by token,
-            timestamp desc
-    ),
-    price_7 as (
-        select distinct on(token) token,
-            price as price_7,
-            timestamp as timestamp_7
-        from oracles.prices
-        where timestamp < current_date - interval '7 days'
-            and timestamp > current_date - interval '8 days'
-        order by token,
-            timestamp desc
-    ),
-    price_30 as (
-        select distinct on(token) token,
-            price as price_30,
-            timestamp as timestamp_30
-        from oracles.prices
-        where timestamp < current_date - interval '30 days'
-            and timestamp > current_date - interval '31 days'
-        order by token,
-            timestamp desc
-    ),
-    price_90 as (
-        select distinct on(token) token,
-            price as price_90,
-            timestamp as timestamp_90
-        from oracles.prices
-        where timestamp < current_date - interval '90 days'
-            and timestamp > current_date - interval '91 days'
-        order by token,
-            timestamp desc
-    )
-    select t.token,
-        price.price,
-        price.timestamp,
-        price_7.price_7,
-        price_7.timestamp_7,
-        price_30.price_30,
-        price_30.timestamp_30,
-        price_90.price_90,
-        price_90.timestamp_90
-    from token t
-        left join price on t.token = price.token
-        left join price_7 on t.token = price_7.token
-        left join price_30 on t.token = price_30.token
-        left join price_90 on t.token = price_90.token
-);
+create view api.historic_token_prices as with token as (
+    select distinct token
+    from oracles.prices
+    where timestamp > CURRENT_DATE - 92
+),
+price as (
+    select distinct on(token) token,
+        avg(price) as price
+
+    from oracles.prices
+    where timestamp > current_date - interval '3 days'
+    group by token
+),
+price_7 as (
+    select distinct on(token) token,
+        avg(price)  as price
+
+    from oracles.prices
+    where
+    timestamp < current_date - interval '6 days'
+    and timestamp > current_date - interval '8 days'
+    group by token
+),
+price_30 as (
+    select distinct on(token) token,
+        avg(price) as price
+
+    from oracles.prices
+    where timestamp < current_date - interval '29 days'
+    and timestamp > current_date - interval '31 days'
+    group by token
+),
+price_90 as (
+select distinct on(token) token,
+        avg(price) as price
+
+    from oracles.prices
+	where timestamp < current_date - interval '89 days'
+    and timestamp > current_date - interval '91 days'
+    group by token
+)
+select t.token,
+    price.price,
+    price_7.price as price_7,
+    price_30.price as price_30,
+    price_90.price as price_90
+from token t
+    left join price on t.token = price.token
+    left join price_7 on t.token = price_7.token
+    left join price_30 on t.token = price_30.token
+    left join price_90 on t.token = price_90.token;
