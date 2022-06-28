9e7f1ecfc8f87fa47cf0a34a8940b49ca8165a0d
Jakub Swierczek <jakub.swierczek@outlook.com>
2022-06-28 19:34:30 +0200

    token_price type & changes in the routine

--- src/borrow/migrations/031-maker-oracle-token-prices.sql
+++ src/borrow/migrations/031-maker-oracle-token-prices.sql
@@ -1,14 +1,83 @@
-create or replace function api.maker_oracle_token_prices(token character varying, date date)
-    returns TABLE(token character varying, price numeric, date date)
-    language sql
+create type api.token_price as (token varchar(32), price numeric(78, 18), timestamp timestamp);
+
+create or replace function api.maker_oracle_token_prices (token character varying, date timestamp default now())
+    returns api.token_price
+    language plpgsql
 as
 $$
-select
+declare
+    found_item api.token_price;
+begin
+    select
         p.token,
         p.price,
-        p.timestamp::date as date
+        p.timestamp as date
+    from oracles.prices p
+    where p.token = $1 and p.price > 0 and
+        p.timestamp between $2 and $2 + interval '1 day'
+    order by abs(extract(epoch from p.timestamp - $2))
+        fetch first 1 rows only
+    into found_item;
+
+    if found_item is not null then
+        return found_item;
+    end if;
+
+    select
+        p.token,
+        p.price,
+        p.timestamp as date
+    from oracles.prices p
+    where p.token = $1 and p.price > 0 and
+        p.timestamp between $2 and $2 + interval '5 day'
+    order by abs(extract(epoch from p.timestamp - $2))
+        fetch first 1 rows only
+    into found_item;
+
+    if found_item is not null then
+        return found_item;
+    end if;
+
+    select
+        p.token,
+        p.price,
+        p.timestamp as date
+    from oracles.prices p
+    where p.token = $1 and p.price > 0 and
+        p.timestamp::date between $2 and $2 + interval '30 day'
+    order by abs(extract(epoch from p.timestamp - $2))
+        fetch first 1 rows only
+    into found_item;
+
+    if found_item is not null then
+        return found_item;
+    end if;
+
+    select
+        p.token,
+        p.price,
+        p.timestamp as date
+    from oracles.prices p
+    where p.token = $1 and p.price > 0 and
+        p.timestamp between $2 and $2 + interval '180 day'
+    order by abs(extract(epoch from p.timestamp - $2))
+        fetch first 1 rows only
+    into found_item;
+
+    if found_item is not null then
+        return found_item;
+    end if;
+
+    select
+        p.token,
+        p.price,
+        p.timestamp as date
     from oracles.prices p
     where p.token = $1 and p.price > 0
-    order by abs(p.timestamp::date - $2)
-    fetch first 1 rows only
+    order by abs(extract(epoch from p.timestamp - $2))
+        fetch first 1 rows only
+    into found_item;
+
+    return found_item;
+end;
 $$;
