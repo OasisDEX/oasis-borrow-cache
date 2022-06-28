ee2a4eb926de1a30ae57c25a29778ac8e19247d5
Jakub Swierczek <jakub.swierczek@outlook.com>
2022-06-28 20:10:45 +0200

    inner function

--- src/borrow/migrations/031-maker-oracle-token-prices.sql
+++ src/borrow/migrations/031-maker-oracle-token-prices.sql
@@ -1,22 +1,40 @@
 create type api.token_price as (token varchar(32), price numeric(78, 18), timestamp timestamp);
 
-create or replace function api.maker_oracle_token_prices (token character varying, date timestamp default now())
-    returns api.token_price
+create or replace function api.maker_oracle_token_prices(token character varying, date timestamp without time zone DEFAULT now()) returns api.token_price
     language plpgsql
 as
 $$
 declare
     found_item api.token_price;
 begin
+    create or replace function token_price_interval(token varchar(32), date timestamp, days interval)
+        returns api.token_price
+        language plpgsql
+    as
+    $inner$
+    declare
+        result api.token_price;
+    begin
+        select
+            p.token,
+            p.price,
+            p.timestamp
+        from oracles.prices p
+        where p.token = $1
+          and p.timestamp between $2 - days and $2 + days
+        order by abs(extract(epoch from p.timestamp - $2))
+            fetch first 1 rows only
+        into result;
+
+        return result;
+    end;
+    $inner$;
+
     select
-        p.token,
-        p.price,
-        p.timestamp as date
-    from oracles.prices p
-    where p.token = $1 and p.price > 0 and
-        p.timestamp between $2 and $2 + interval '1 day'
-    order by abs(extract(epoch from p.timestamp - $2))
-        fetch first 1 rows only
+        r.token,
+        r.price,
+        r.timestamp
+    from token_price_interval(token, date, interval '1 day') r
     into found_item;
 
     if found_item is not null then
@@ -24,14 +42,10 @@ begin
     end if;
 
     select
-        p.token,
-        p.price,
-        p.timestamp as date
-    from oracles.prices p
-    where p.token = $1 and p.price > 0 and
-        p.timestamp between $2 and $2 + interval '5 day'
-    order by abs(extract(epoch from p.timestamp - $2))
-        fetch first 1 rows only
+        r.token,
+        r.price,
+        r.timestamp
+    from token_price_interval(token, date, interval '5 day') r
     into found_item;
 
     if found_item is not null then
@@ -39,14 +53,10 @@ begin
     end if;
 
     select
-        p.token,
-        p.price,
-        p.timestamp as date
-    from oracles.prices p
-    where p.token = $1 and p.price > 0 and
-        p.timestamp::date between $2 and $2 + interval '30 day'
-    order by abs(extract(epoch from p.timestamp - $2))
-        fetch first 1 rows only
+        r.token,
+        r.price,
+        r.timestamp
+    from token_price_interval(token, date, interval '30 day') r
     into found_item;
 
     if found_item is not null then
@@ -54,14 +64,10 @@ begin
     end if;
 
     select
-        p.token,
-        p.price,
-        p.timestamp as date
-    from oracles.prices p
-    where p.token = $1 and p.price > 0 and
-        p.timestamp between $2 and $2 + interval '180 day'
-    order by abs(extract(epoch from p.timestamp - $2))
-        fetch first 1 rows only
+        r.token,
+        r.price,
+        r.timestamp
+    from token_price_interval(token, date, interval '180 day') r
     into found_item;
 
     if found_item is not null then
