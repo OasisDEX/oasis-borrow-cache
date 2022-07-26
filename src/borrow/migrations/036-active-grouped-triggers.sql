f34ee879cd626f24045b475bfe209843da7036b3
halaprix <panicz.klos@gmail.com>
2022-07-26 13:19:57 +0200

    drop it to add `group_id`

--- src/borrow/migrations/036-active-grouped-triggers.sql
+++ src/borrow/migrations/036-active-grouped-triggers.sql
@@ -1,3 +1,13 @@
+DROP VIEW api.active_triggers;
+CREATE VIEW api.active_triggers as
+SELECT added.*
+FROM automation_bot.trigger_added_events added
+     LEFT JOIN automation_bot.trigger_removed_events removed ON added.trigger_id = removed.trigger_id
+     LEFT JOIN automation_bot.trigger_executed_events executed ON added.trigger_id = executed.trigger_id
+     LEFT JOIN automation_bot.trigger_group_added groupped ON added.trigger_id = groupped.trigger_id
+WHERE removed.trigger_id IS NULL
+     AND executed.trigger_id IS NULL;
+     
 CREATE VIEW api.active_trigger_groups as
 select group_added.group_id,
      group_added.group_type,
