d660f611241ebf28143c5cdd12acb6a0e64f84a3
halaprix <panicz.klos@gmail.com>
2022-08-01 13:17:05 +0200

    format

--- src/borrow/migrations/035-automation-aggregator-bot.sql
+++ src/borrow/migrations/035-automation-aggregator-bot.sql
@@ -43,39 +43,39 @@ FROM automation_bot.trigger_added_events added
 WHERE removed.trigger_id IS NULL
     AND executed.trigger_id IS NULL;
 CREATE VIEW api.active_trigger_groups as
-select b.group_id,
+SELECT b.group_id,
     a.group_type,
     b.cdp_id,
     array_agg(b.trigger_id) trigger_ids,
     array_agg(
         b.trigger_type
-        order by b.trigger_type
+        ORDER BY b.trigger_type
     ) trigger_types,
     array_agg(b.command_address) trigger_commands
-from automation_bot.trigger_added_events b
-    left join automation_aggregator_bot.trigger_group_added_events a on a.group_id = b.group_id
-    left join automation_bot.trigger_removed_events r ON b.trigger_id = r.trigger_id
-    left join automation_bot.trigger_executed_events e ON b.trigger_id = e.trigger_id
-where r.trigger_id is null
-    AND e.trigger_id is null
-    and b.group_id is not null
-group by a.group_type,
+FROM automation_bot.trigger_added_events b
+    LEFT JOIN automation_aggregator_bot.trigger_group_added_events a ON a.group_id = b.group_id
+    LEFT JOIN automation_bot.trigger_removed_events r ON b.trigger_id = r.trigger_id
+    LEFT JOIN automation_bot.trigger_executed_events e ON b.trigger_id = e.trigger_id
+WHERE r.trigger_id IS NULL
+    AND e.trigger_id IS NULL
+    AND b.group_id IS NOT NULL
+GROUP BY a.group_type,
     b.group_id,
     b.cdp_id
-order by b.group_id asc;
+ORDER BY b.group_id asc;
 CREATE VIEW api.active_groupped_triggers AS
-select b.group_id,
+SELECT b.group_id,
     a.group_type,
     b.cdp_id,
     b.trigger_id,
     b.trigger_type,
     b.command_address,
     b.trigger_data
-from automation_bot.trigger_added_events b
-    left join automation_aggregator_bot.trigger_group_added_events a on a.group_id = b.group_id
-    left join automation_bot.trigger_removed_events r ON b.trigger_id = r.trigger_id
-    left join automation_bot.trigger_executed_events e ON b.trigger_id = e.trigger_id
-where r.trigger_id is null
-    AND e.trigger_id is null
-    and b.group_id is not null
-order by b.group_id asc;
+FROM automation_bot.trigger_added_events b
+    LEFT JOIN automation_aggregator_bot.trigger_group_added_events a ON a.group_id = b.group_id
+    LEFT JOIN automation_bot.trigger_removed_events r ON b.trigger_id = r.trigger_id
+    LEFT JOIN automation_bot.trigger_executed_events e ON b.trigger_id = e.trigger_id
+WHERE r.trigger_id IS NULL
+    AND e.trigger_id IS NULL
+    AND b.group_id IS NOT NULL
+ORDER BY b.group_id asc;
