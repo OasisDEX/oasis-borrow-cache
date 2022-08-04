f48f2ccf8b5b2e6e48d17a4dd55929b8e00eac5b
sennett <anthony@oazoapps.com>
2022-08-04 17:21:10 +0100

    add some more notes

--- src/borrow/migrations/037-add-reserve-column-to-aave-reserve.sql
+++ src/borrow/migrations/037-add-reserve-column-to-aave-reserve.sql
@@ -1,1 +1,7 @@
+-- truncate data from aave.reserve_data_updated
+
+-- add new column similar to this:
+
 alter table aave.reserve_data_updated add column reserve bytea not null;
+
+-- delete from vulcan2x/jon WHERE name like 'aave-lending-pool-transformer%'
