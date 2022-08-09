60efd55073c414c182f0389ba4a2a81206a24373
sebastianPiekarczyk <sebastian@oazoapps.com>
2022-08-09 09:31:33 +0200

    spacings fix

--- src/borrow/migrations/037-group-id-in-trigger-events-view.sql
+++ src/borrow/migrations/037-group-id-in-trigger-events-view.sql
@@ -9,7 +9,7 @@ CREATE VIEW api.trigger_events AS (
 	LEFT JOIN vulcan2x.block b
 	ON b.id = tae.block_id
 	UNION
-	SELECT  tre.id , tre.trigger_id , tre.cdp_id, tre.log_index , tx.hash , b."number", b."timestamp" ,'removed' AS event_type, tae.command_address, alias.kind, tae.trigger_data, tae.group_id  from automation_bot.trigger_removed_events tre
+	SELECT  tre.id , tre.trigger_id , tre.cdp_id, tre.log_index , tx.hash , b."number", b."timestamp" ,'removed' AS event_type, tae.command_address, alias.kind, tae.trigger_data, tae.group_id from automation_bot.trigger_removed_events tre
 	LEFT JOIN automation_bot.trigger_added_events  tae
 	ON tre.trigger_id = tae.trigger_id
 	LEFT JOIN automation_bot.command_alias alias
@@ -19,7 +19,7 @@ CREATE VIEW api.trigger_events AS (
 	LEFT JOIN vulcan2x.block b
 	ON b.id = tre.block_id
 	UNION
-	SELECT  tee.id , tee.trigger_id , tee.cdp_id, tee.log_index , tx.hash , b."number", b."timestamp" ,'executed' AS event_type, tae.command_address,  alias.kind, tae.trigger_data, tae.group_id  from automation_bot.trigger_executed_events tee
+	SELECT  tee.id , tee.trigger_id , tee.cdp_id, tee.log_index , tx.hash , b."number", b."timestamp" ,'executed' AS event_type, tae.command_address,  alias.kind, tae.trigger_data, tae.group_id from automation_bot.trigger_executed_events tee
 	LEFT JOIN automation_bot.trigger_added_events  tae
 	ON tee.trigger_id = tae.trigger_id
 	LEFT JOIN automation_bot.command_alias alias
