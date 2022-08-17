b8e5f36a524da6af97587454a80c1fff6ef39c26
halaprix <panicz.klos@gmail.com>
2022-08-17 14:25:38 +0200

    revert history

--- src/types/history.ts
+++ src/types/history.ts
@@ -137,19 +137,6 @@ interface MigrateEvent extends HistoryEventBase {
   kind: 'MIGRATE';
 }
 
-interface AddTrigger extends HistoryEventBase {
-  kind: 'ADD_TRIGGER';
-  trigger_id: string;
-  cdp_id: string;
-  command_address: string;
-  trigger_data: string;
-}
-interface RemoveTrigger extends HistoryEventBase {
-  kind: 'REMOVE_TRIGGER';
-  trigger_id: string;
-  cdp_id: string;
-}
-
 export type Event =
   | VaultOpenedEvent
   | WithdrawEvent
@@ -165,6 +152,4 @@ export type Event =
   | AuctionStartedEvent
   | WithdrawPaybackEvent
   | DepositGenerateEvent
-  | PaybackEvent
-  | AddTrigger
-  | RemoveTrigger;
+  | PaybackEvent;
