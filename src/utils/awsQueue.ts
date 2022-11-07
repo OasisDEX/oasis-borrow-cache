61d7109f74e5aa50133d1988a1f74af52b2f299a
halaprix <panicz.klos@gmail.com>
2022-11-07 16:43:01 +0100

    log

--- src/utils/awsQueue.ts
+++ src/utils/awsQueue.ts
@@ -11,12 +11,14 @@ export enum MessageTypes {
 
 function getAWS() {
   if (process.env.AWS_SQS == 'production') {
-    AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
+    const aws = new AWS.SQS({ apiVersion: '2012-11-05' });
+    aws.config.getCredentials(function(err) {
+      if (err) console.log(err.stack);
+      else {
+        console.log('Access key:', AWS?.config?.credentials?.accessKeyId);
+      }
     });
-    return new AWS.SQS({ apiVersion: '2012-11-05' });
+    return aws;
   } else {
     const aws = {
       sendMessage: (
