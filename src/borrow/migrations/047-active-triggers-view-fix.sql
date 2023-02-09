be8f5198af8990d2ad8e87550fe569ee60af54a9
sebastianPiekarczyk <sebastian@oazoapps.com>
2023-02-09 13:55:46 +0100

    remove where

--- src/borrow/migrations/047-active-triggers-view-fix.sql
+++ src/borrow/migrations/047-active-triggers-view-fix.sql
@@ -5,4 +5,4 @@ CREATE VIEW api.active_triggers as
     FROM automation_bot.trigger_added_events added
     LEFT JOIN automation_bot.trigger_removed_events removed ON added.trigger_id = removed.trigger_id
     LEFT JOIN automation_bot.trigger_executed_events executed ON added.trigger_id = executed.trigger_id
-    WHERE removed.trigger_id is null AND WHERE executed.trigger_id is null;
+    WHERE removed.trigger_id is null AND executed.trigger_id is null;
